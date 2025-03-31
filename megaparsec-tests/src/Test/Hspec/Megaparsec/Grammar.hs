ea607d54acf693bbf68183a1e0fcbea1e2201800
Alec Ghost <alexander.ghost@web.de>
2025-03-31 13:17:48 +0200

    Remove startDifferent export and add doc comments

--- megaparsec-tests/src/Test/Hspec/Megaparsec/Grammar.hs
+++ megaparsec-tests/src/Test/Hspec/Megaparsec/Grammar.hs
@@ -1,4 +1,6 @@
--- | This module provides a way to generate arbitrary grammars and convert them --   to Megaparsec parsers and to QuickCheck input generators that the parser --   can parse completely.
+-- | This module provides a way to generate arbitrary grammars and convert them
+-- to Megaparsec parsers and to QuickCheck input generators that the parser
+-- can parse completely.
 module Test.Hspec.Megaparsec.Grammar
   ( -- * Grammar data type
     Grammar (..),
@@ -6,9 +8,6 @@ module Test.Hspec.Megaparsec.Grammar
     -- * Grammar converter functions
     toParsec,
     toInputGen,
-
-    -- * Grammar analysis functions
-    startDifferent,
   )
 where
 
@@ -73,7 +72,8 @@ toParsec (Choice gs) = MP.choice (fmap (MP.try . toParsec) gs)
 toParsec (Sequence gs) = concat <$> mapM toParsec gs
 
 -- Obtain an input generator for the given grammar.
--- If the
+-- If the properties listed in the docs of `arbitraryGrammar` are fulfilled
+-- the generated input is always fully consumed.
 toInputGen :: (Arbitrary t, Ord t) => Grammar t -> Gen [t]
 toInputGen AnyToken = (: []) <$> arbitrary
 toInputGen (Token t) = return [t]
@@ -85,50 +85,28 @@ toInputGen (Many g) = concat <$> (reps >>= flip vectorOf (toInputGen g))
 toInputGen (Choice gs) = oneof (map toInputGen gs)
 toInputGen (Sequence gs) = concat <$> mapM toInputGen gs
 
--- Determine whether the two grammars start with a disjoint set of first tokens.
-startDifferent :: (Eq t) => Grammar t -> Grammar t -> Bool
-startDifferent g1 g2 = disjointSets set1 set2
-  where
-    set1 = findFirstToken g1
-    set2 = findFirstToken g2
-
-    tokenEq _ AnyToken = True
-    tokenEq AnyToken _ = True
-    tokenEq a b = a == b
-
-    disjointSets s1 s2 =
-      isNothing $ find (\t1 -> isJust $ find (\t2 -> tokenEq t1 t2) s1) s2
-
--- Find the set of first tokens that can be recognized by the grammar.
--- Returns a list of `Token` and `AnyToken`.
-findFirstToken :: Grammar t -> [Grammar t]
-findFirstToken t@(Token _) = [t]
-findFirstToken t@AnyToken = [t]
-findFirstToken (Choice gs) = concatMap findFirstToken gs
-findFirstToken (Sequence []) = []
-findFirstToken (Sequence (g : gs)) =
-  findFirstToken g
-    ++ if (not . consumesInput) g
-      then findFirstToken (Sequence gs)
-      else []
-findFirstToken (Many g) = findFirstToken g
-
--- Whether the grammar might not consume input
-consumesInput :: Grammar t -> Bool
-consumesInput (Many _) = False
-consumesInput (Token _) = True
-consumesInput AnyToken = True
-consumesInput (Sequence gs) = any consumesInput gs
-consumesInput (Choice gs) = all consumesInput gs
-
+-- | Generator for arbitrary grammars that fulfill the following properties:
+-- - Do not contain repetitions that recognize the empty string
+--   (the parser would loop endless)
+-- - Do not contain the repetition of a string followed by that string
+--   (the parser would always fail)
+-- - Do not contain a choice where a preceeding option recognizes a prefix
+--   of what a succeeding option recognizes
+--   (the parser would never use the later choice)
+--
+-- Furthermore, the current implementation never generates grammars
+-- that end in a repetition.
+-- Due to this, if a grammar recognizes a string,
+-- it will not recognize this string appended with a suffix,
+-- which is useful for the usage in the test suite.
 arbitraryGrammar :: (Arbitrary t, Eq t) => Int -> Gen (Grammar t)
 arbitraryGrammar size | size <= 1 = Token <$> arbitrary
 arbitraryGrammar size =
   frequency
     [ (1, return AnyToken),
       (1, Token <$> arbitrary),
-      (1, some <$> suchThat (vectorOf 2 subGrammar) areDifferent),
-      (1, Choice <$> suchThat (vectorOf 2 subGrammar) areDifferent),
+      (1, some <$> suchThat (vectorOf 2 subGrammar) startDifferent),
+      (1, Choice <$> suchThat (vectorOf 2 subGrammar) startDifferent),
       (1, Sequence <$> vectorOf 2 subGrammar)
     ]
   where
@@ -137,8 +115,39 @@ arbitraryGrammar size =
 
     -- A sequence of many g1 and g2
     some [g1, g2] = Sequence [Many g1, g2]
-    some _ = error "Some takes two subgrammars"
+    some _ = error "some takes two subgrammars"
 
-    -- Call `startDifferent` with the two grammars
-    areDifferent [g1, g2] = startDifferent g1 g2
-    areDifferent _ = error "choice must have two options"
+    -- Determine whether the two grammars
+    -- start with a disjoint set of first tokens.
+    startDifferent [g1, g2] = disjointSets set1 set2
+      where
+        set1 = findFirstToken g1
+        set2 = findFirstToken g2
+
+        tokenEq _ AnyToken = True
+        tokenEq AnyToken _ = True
+        tokenEq a b = a == b
+
+        disjointSets s1 s2 =
+          isNothing $ find (\t1 -> isJust $ find (\t2 -> tokenEq t1 t2) s1) s2
+    startDifferent _ = error "startDifferent takes two subgrammars"
+
+    -- Find the set of first tokens that can be recognized by the grammar.
+    -- Returns a list of `Token` and `AnyToken`.
+    findFirstToken t@(Token _) = [t]
+    findFirstToken t@AnyToken = [t]
+    findFirstToken (Choice gs) = concatMap findFirstToken gs
+    findFirstToken (Sequence []) = []
+    findFirstToken (Sequence (g : gs)) =
+      findFirstToken g
+        ++ if (not . consumesInput) g
+          then findFirstToken (Sequence gs)
+          else []
+    findFirstToken (Many g) = findFirstToken g
+
+    -- Whether the grammar might not consume input
+    consumesInput (Many _) = False
+    consumesInput (Token _) = True
+    consumesInput AnyToken = True
+    consumesInput (Sequence gs) = any consumesInput gs
+    consumesInput (Choice gs) = all consumesInput gs
